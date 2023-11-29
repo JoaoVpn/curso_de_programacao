bc0f6d7d9a2288f1994f5e3628bb34f882fa92c7
JoaoVpn <johnvitor6018@gmail.com>
2023-11-28 22:06:06 -0300

    Alterações no código.
    Aprendizado da função print e println.

--- src/main.java
+++ src/main.java
@@ -2,8 +2,8 @@
 public class main {
 
 	public static void main(String[] args) {
-		System.out.println("Olá, mundo!");
-
+		System.out.print("Olá, mundo!");
+		System.out.println("Bom dia!");
 	}
 
 }
