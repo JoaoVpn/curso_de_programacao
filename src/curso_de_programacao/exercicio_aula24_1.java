5949b93f6da828566e9dd44971f56914eae035a3
JoaoVpn <johnvitor6018@gmail.com>
2023-11-30 00:02:52 -0300

    Atualizações da aula 24.

---
+++ src/curso_de_programacao/exercicio_aula24_1.java
@@ -0,0 +1,17 @@
+package curso_de_programacao;
+
+public class exercicio_aula24_1 {
+
+	public static void main(String[] args) {
+		
+			int a, b;
+			double resultado;
+			a = 5;
+			b = 2;
+			resultado = (double) a / b; // O "Casting" é a conversão explícita dos valores. Nesse caso foi de int para double.
+			
+			System.out.println(resultado);
+
+		}
+
+	}
