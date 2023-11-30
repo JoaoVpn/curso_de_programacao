1f6ab31ea9e12f2faed8bb42ccc88a7863eaaf50
JoaoVpn <johnvitor6018@gmail.com>
2023-11-29 23:48:32 -0300

    Atualizações da aula 24

---
+++ src/curso_de_programacao/exercicio_aula24.java
@@ -0,0 +1,18 @@
+package curso_de_programacao;
+
+public class exercicio_aula24 {
+
+	public static void main(String[] args) {
+		// Uma boa prática recomendada em aula é especificar no valor da variável o tipo dela. Por exemplo, variável do tipo float "variavel = 9f". Variável do tipo double "variavel = 9.0"
+		
+		double b, B, h, area;
+		b = 6.0;
+		B = 8.0;
+		h = 5.0;
+		
+		area = (b + B) / 2.0 * h;
+		System.out.println(area);
+		
+	}
+
+}
