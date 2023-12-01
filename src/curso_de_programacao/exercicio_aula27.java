245f76c2b2bffe6791a9b91a9981108773859090
JoaoVpn <johnvitor6018@gmail.com>
2023-12-01 00:37:48 -0300

    Atualizações da aula 27.

---
+++ src/curso_de_programacao/exercicio_aula27.java
@@ -0,0 +1,22 @@
+package curso_de_programacao;
+
+public class exercicio_aula27 {
+
+	public static void main(String[] args) {
+		Double x, y, z; // Declara as variáveis x, y e z e as coloca como Double
+		
+		Double a = -5.0;
+		Double b = 4.0;
+		Double c = 2.0;
+	
+
+		x = Math.abs(a); // Armazena em "x" o valor absoluto de "a"
+		y = Math.sqrt(b); // Armazena em "y" a raiz quadrada de "b"
+		z = Math.pow(b, c); // Armazena a potênciação de "b" em "c" Ex: 4^2
+
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+	}
+
+}
