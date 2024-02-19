5002eb61f833b3e441d1c9cbc9cd55167dcb9c05
JoaoVpn <johnvitor6018@gmail.com>
2024-02-18 22:33:56 -0300

    Atualização da aula 37.

---
+++ src/curso_de_programacao/exercicio_aula37.java
@@ -0,0 +1,23 @@
+package curso_de_programacao;
+
+import java.util.Scanner;
+import java.util.Locale;
+
+public class exercicio_aula37 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Quantidade de minutos gastos: ");
+        int minutos = sc.nextInt();
+        double conta = 50.0;
+        if (minutos <= 100) {
+            System.out.println("Valor a pagar: R$ 50,00");
+        } else if (minutos > 100) {
+            conta = conta + (minutos - 100) * 2.0;
+            System.out.printf("Valor a pagar: R$ %.2f", conta);    
+        }
+        
+        sc.close();
+    }
+}
